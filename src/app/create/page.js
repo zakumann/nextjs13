01817619a142faaeed69046a2200bbbd9b3b7be6
zakumann <samuel742able@gmail.com>
2023-09-18 22:48:57 +0900

    Thank you egoing

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -4,25 +4,30 @@ import { useRouter } from "next/navigation";
   
 export default function Create(){
   const router = useRouter();
-  return <form onSubmit={async evt=>{
-    evt.preventDefault();
-    const title = evt.target.title.value;
-    const body = evt.target.body.value;
-    const resp = await fetch('http://localhost:9999/topics/', {
+  return (
+  <form onSubmit={(e)=>{
+    e.preventDefault();
+    const title = e.target.title.value;
+    const body = e.target.body.value;
+    const options = {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({title, body})
-    });
-    const topic = await resp.json();
-    console.log("file: page.js:19 ~ Create ~ topic:", topic)
-    router.push(`/read/${topic.id}`);
-    router.refresh();
+    }
+    fetch(process.env.NEXT_PUBLIC_API_URL+'topics', options)
+    .then(res=>res.json())
+    .then(result=>{
+      console.log(result);
+      const lastid = result.id;
+      router.refresh();
+      router.push(`/read/${lastid}`);
+    })
   }}>
     <h2>Create</h2>
     <p><input type="text" name="title" placeholder="title" /></p>
     <p><textarea name="body" placeholder="body"></textarea></p>
     <p><input type="submit" value="create" /></p>
   </form>
-}
+)}
--- src/app/update/[id]/page.js
+++ src/app/update/[id]/page.js
@@ -8,7 +8,7 @@ export default function Update(props){
   const [body, setBody] = useState('');
   const router = useRouter();
   const params = useParams();
-  const id = props.params;
+  const id = params.id;
   useEffect(()=>{
     fetch('http://localhost:9999/topics/'+id)
     .then(resp=>resp.json())
@@ -17,26 +17,29 @@ export default function Update(props){
         setBody(result.body);
     });
   }, []);
-  return (<form onSubmit={(e)=>{
-    e.preventDefault();
-    const title = e.target.title.value;
-    const body = e.target.body.value;
-    const options = {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({title, body})
-    }
-    fetch('http://localhost:9999/topics/'+id, options)
-    .then(res=>res.json())
-    .then(result=>{
-        console.log(result);
-        const lastid = result.id;
-        router.refresh();
-        router.push(`/read/${lastid}`);
-    })
-  }}>
+  return (
+  <form
+    onSubmit={(e)=>{
+      e.preventDefault();
+      const title = e.target.title.value;
+      const body = e.target.body.value;
+      const options = {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({title, body})
+      }
+      fetch(process.env.NEXT_PUBLIC_API_URL+'topics/'+id, options)
+      .then(res=>res.json())
+      .then(result=>{
+          console.log(result);
+          const lastid = result.id;
+          router.refresh();
+          router.push(`/read/${lastid}`);
+        })
+      }}
+    >
     <p>
         <input
             type="text"
